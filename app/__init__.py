26311cfc37986a8a703425c2ec2bc17b0db0301d
JesseStuy20 <jxie20@stuy.edu>
2021-12-10 09:20:25 -0500

    Added database table creation

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,19 @@ import sqlite3
 
 app = Flask(__name__)
 
+DB_FILE="highsteaks.db"
 
+db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+create = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, points INTEGER, wins INTEGER, losses INTEGER, activePFP TEXT) " # create users table
+c.execute((create))
+create = "CREATE TABLE IF NOT EXISTS items (name TEXT, imageUrl TEXT, owner TEXT) " # create items table
+c.execute((create))
+create = "CREATE TABLE IF NOT EXISTS market (name TEXT, imageUrl TEXT, price INTEGER) " # create market table
+c.execute((create))
+
+db.commit() #save changes to db
 
 # Homepage render function
 @app.route("/")
