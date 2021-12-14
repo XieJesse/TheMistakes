f97190eefd0ba324f8ffcc9655496894e9030304
Alif-Abdullah0 <alifabdullah862@gmail.com>
2021-12-14 09:09:29 -0500

    newDeck and drawCards merge

--- app/__init__.py
+++ app/__init__.py
@@ -39,6 +39,7 @@ def is_logged_in():
 # Homepage render function
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    print(newDeck())
     if is_logged_in():
         return render_template("home.html")
     else:
@@ -109,6 +110,24 @@ def blackjack_win(player_scores):
         if player_scores[i] < 22:
             modified_player_scores.append((i, player_scores[i]))
 
+
+# player_scores will be a list.
+# The first element of that list is the score of the human player.
+# There will then be a variable number of cpu player scores.
+# The last score will be the score of the house.
+# There will always be a minimum of 2 scores.
+# Example: [20, 4, 17, 23, 21]
+# Returns list of tuples, where the first element of each tuple is the player number
+# (0 being human, len-1 being cpu, everything else being cpu)
+# and the second element will be the player's score. Everyone over 21 will be removed
+# from the final list. The player with the highest score will win. If two people tie
+# or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
+def blackjackWin(player_scores):
+    modified_player_scores = []
+    for i in range(len(player_scores)):
+        if player_scores[i] < 22:
+            modified_player_scores.append((i, player_scores[i]))
+
 def newDeck():
     # opens up API data (API data being a randomly made deck)
     req = urllib.request.Request('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', headers={'User-Agent': 'Mozilla/5.0'}) #change deck count for more decks of 52
@@ -119,13 +138,11 @@ def newDeck():
     response_info = json.loads(response)
     # sets a variable deckid equal to the deck_id of the drawn deck
     deckid = response_info["deck_id"]
-    return deckid
-
-def drawCards(id):
-    data = urllib.request.urlopen('https://deckofcardsapi.com/api/deck/' + id + '/draw/?count=2')
-    response = data.read()
-    response_info = json.loads(response)
-    cards = response_info["cards"]
+    deck_req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/draw/?count=52', headers={'User-Agent': 'Mozilla/5.0'})
+    deck_data = urllib.request.urlopen(deck_req)
+    deck_response = deck_data.read()
+    deck_dict = json.loads(deck_response)
+    return deck_dict
 
 if __name__ == "__main__":
     app.debug = True
