307262ab1e3502fc29de885f4e8d309ef227686f
xVany <rayy12204@gmail.com>
2021-12-15 08:56:54 -0500

    cleaning up and returncards func

--- app/__init__.py
+++ app/__init__.py
@@ -103,6 +103,7 @@ def login():
     else:
         return render_template("login.html")
 
+# player_scores will be a lis
 # player_scores will be a list.
 # The first element of that list is the score of the human player.
 # There will then be a variable number of cpu player scores.
@@ -120,7 +121,15 @@ def blackjack_win(player_scores):
         if player_scores[i] < 22:
             modified_player_scores.append((i, player_scores[i]))
 
-
+t.
+# The first element of that list is the score of the human player.
+# There will then be a variable number of cpu player scores.
+# The last score will be the score of the house.
+# There will always be a minimum of 2 scores.
+# Example: [20, 4, 17, 23, 21]
+# Returns list of tuples, where the first element of each tuple is the player number
+# (0 being human, len-1 being cpu, everything else being cpu)
+# and the second element will
 # player_scores will be a list.
 # The first element of that list is the score of the human player.
 # There will then be a variable number of cpu player scores.
@@ -130,6 +139,15 @@ def blackjack_win(player_scores):
 # Returns list of tuples, where the first element of each tuple is the player number
 # (0 being human, len-1 being cpu, everything else being cpu)
 # and the second element will be the player's score. Everyone over 21 will be removed
+# from the final list. The player with the highest score will win. If two people tie
+# or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
+def blackjack_win(player_scores):
+    modified_player_scores = []
+    for i in range(len(player_scores)):
+        if player_scores[i] < 22:
+            modified_player_scores.append((i, player_scores[i]))
+
+ be the player's score. Everyone over 21 will be removed
 # from the final list. The player with the highest score will win. If two people tie
 # or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
 def blackjackWin(player_scores):
@@ -154,11 +172,10 @@ def newDeck():
     deck_dict = json.loads(deck_response)
     return deck_dict #Dictionary of lists of dictionary
 
-def shuffle():
-    req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/pile/<<pile_name>>/shuffle/', headers={'User-Agent': 'Mozilla/5.0'})
+def returnCards():
+    req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + DECKID + '/return/', headers={'User-Agent': 'Mozilla/5.0'})
     data = urllib.request.urlopen(req)
-    response = data.read()
-    deck_dict = json.loads(response)
+    #returns all cards back to deck and shuffles
 
 if __name__ == "__main__":
     app.debug = True
