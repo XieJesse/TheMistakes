321f22e499fc28ace0f2f7702a26087596cda092
Alif-Abdullah0 <alifabdullah862@gmail.com>
2021-12-16 09:10:14 -0500

    modified game.py and devlog.txt

--- app/game.py
+++ app/game.py
@@ -12,23 +12,6 @@ import sqlite3, json, urllib
 # and the second element will be the player's score. Everyone over 21 will be removed
 # from the final list. The player with the highest score will win. If two people tie
 # or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
-def blackjack_win(player_scores):
-    modified_player_scores = []
-    for i in range(len(player_scores)):
-        if player_scores[i] < 22:
-            modified_player_scores.append((i, player_scores[i]))
-
-# player_scores will be a list.
-# The first element of that list is the score of the human player.
-# There will then be a variable number of cpu player scores.
-# The last score will be the score of the house.
-# There will always be a minimum of 2 scores.
-# Example: [20, 4, 17, 23, 21]
-# Returns list of tuples, where the first element of each tuple is the player number
-# (0 being human, len-1 being cpu, everything else being cpu)
-# and the second element will be the player's score. Everyone over 21 will be removed
-# from the final list. The player with the highest score will win. If two people tie
-# or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
 
 def blackjackWin(player_scores):
     modified_player_scores = []
@@ -50,14 +33,16 @@ def newDeck():
     deck_data = urllib.request.urlopen(deck_req)
     deck_response = deck_data.read()
     deck_dict = json.loads(deck_response)
-    session["deck"] = deck_dict["cards"] #Dictionary of lists of dictionary
+    session["deck"] = deck_dict["cards"] #List of dictionaries
 
-def drawCards():
-    deck_req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/draw/?count=52', headers={'User-Agent': 'Mozilla/5.0'})
-    deck_data = urllib.request.urlopen(deck_req)
-    deck_response = deck_data.read()
-    deck_dict = json.loads(deck_response)
-    return deck_dict #Dictionary of lists of dictionary
+# IMPORTATNT: returns empty list if 'deck' is not in session.keys()
+def drawCards(numCards):
+    cards = []
+    if ('deck' in session.keys()):
+        for i in range(numCards):
+            cards.append(session['deck'][i])
+        session['deck'] = session['deck'][numCards]
+    return cards
 
 def shuffle():
     req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/pile/<<pile_name>>/shuffle/', headers={'User-Agent': 'Mozilla/5.0'})
