074769423255c1351a1acb3d6b241ab2876cd634
xVany <rayy12204@gmail.com>
2021-12-16 09:11:43 -0500

    cleaning up

--- app/game.py
+++ app/game.py
@@ -1,6 +1,12 @@
 from flask import Flask, Blueprint, request, session, render_template, redirect, g
 import sqlite3, json, urllib
 
+def blackjack_win(player_scores):
+    modified_player_scores = []
+    for i in range(len(player_scores)):
+        if player_scores[i] < 22:
+            modified_player_scores.append((i, player_scores[i]))
+
 # player_scores will be a list.
 # The first element of that list is the score of the human player.
 # There will then be a variable number of cpu player scores.
@@ -10,15 +16,7 @@ import sqlite3, json, urllib
 # Returns list of tuples, where the first element of each tuple is the player number
 # (0 being human, len-1 being cpu, everything else being cpu)
 # and the second element will be the player's score. Everyone over 21 will be removed
-# from the final list. The player with the highest score will win. If two people tie
-# or if the list of tuples is empty (meaning everyone busted) then no one will be awarded coins
-
-def blackjackWin(player_scores):
-    modified_player_scores = []
-    # for i in range(len(player_scores)):
-    #     if player_scores[i] < 22:
-            # modified_player_scores.append((i, player_scores will be a lislayer_scores[i]))
-
+# from the final list. The
 def newDeck():
     # opens up API data (API data being a randomly made deck)
     req = urllib.request.Request('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', headers={'User-Agent': 'Mozilla/5.0'}) #change deck count for more decks of 52
@@ -29,20 +27,12 @@ def newDeck():
     response_info = json.loads(response)
     # sets a variable deckid equal to the deck_id of the drawn deck
     deckid = response_info["deck_id"]
-    deck_req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/draw/?count=52', headers={'User-Agent': 'Mozilla/5.0'})
+    deck_req = urllib.r    data = urllib.request.urlopen(req)
+    #returns all cards back to deck and shufflesequest.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/draw/?count=52', headers={'User-Agent': 'Mozilla/5.0'})
     deck_data = urllib.request.urlopen(deck_req)
     deck_response = deck_data.read()
     deck_dict = json.loads(deck_response)
-    session["deck"] = deck_dict["cards"] #List of dictionaries
-
-# IMPORTATNT: returns empty list if 'deck' is not in session.keys()
-def drawCards(numCards):
-    cards = []
-    if ('deck' in session.keys()):
-        for i in range(numCards):
-            cards.append(session['deck'][i])
-        session['deck'] = session['deck'][numCards]
-    return cards
+    session["deck"] = deck_dict["cards"] #Dictionary of lists of dictionary
 
 def shuffle():
     req = urllib.request.Request('https://deckofcardsapi.com/api/deck/' + deckid + '/pile/<<pile_name>>/shuffle/', headers={'User-Agent': 'Mozilla/5.0'})
