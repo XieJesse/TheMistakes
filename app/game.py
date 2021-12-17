832843151a98638e1bc7896afb1ee164dd7eb8f3
Alif-Abdullah0 <alifabdullah862@gmail.com>
2021-12-17 09:08:57 -0500

    created newGame function to track player stats in newGame

--- app/game.py
+++ app/game.py
@@ -18,6 +18,15 @@ def blackjack_win(player_scores):
         if player_scores[i] < 22:
             modified_player_scores.append((i, player_scores[i]))
 
+# Creates a session variable that is a list of tuples
+# Here is the tuple diagram: (Cards, Card Value, Status)
+# Example tuple: ("K5A", 17, Hit/Bust/Stay)
+def newGame(playerCount):
+    # Session variable players tracks human and cpu player stats
+    session['players'] = [("",0,"") for i in range(playerCount)]
+    # Session variable house tracks house player
+    session['house'] = [("",0,"")]
+
 def newDeck():
     # opens up API data (API data being a randomly made deck)
     req = urllib.request.Request('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', headers={'User-Agent': 'Mozilla/5.0'}) #change deck count for more decks of 52
