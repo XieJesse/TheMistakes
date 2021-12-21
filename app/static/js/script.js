476ec0b1ae5ec6e90dd0746ab6cf14b5a8b04901
sookiemonster <sookiemonster216@gmail.com>
2021-12-21 16:18:04 -0500

    Add card color changing functionality.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // On DOM loaded do this
+
+    // Get the radio inputs
+    var mainRadio = document.setup.mainColor;
+    var altRadio = document.setup.altColor;
+
+    // Get list of cards 
+    var mainCards = document.getElementsByClassName("main-color");
+    var altCards = document.getElementsByClassName("alt-color");
+
+    setCardHandlers(mainRadio, mainCards, "--suit-main");
+    setCardHandlers(altRadio, altCards, "--suit-alt");
 });
 
+function setCardBase(cards, lightness) {
+    // Adjust the card's base color if the suit color is too light
+    if (lightness > 159) {
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].classList.add("dark");
+        }
+    } else {
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].classList.remove("dark");
+        }
+    }
+}
+
+
+function lightness(channels) {
+    // Get the lightness component of an RGB color
+    r = channels[0];
+    g = channels[1];
+    b = channels[2];
+    return (Math.max(r,g,b) + Math.min(r,g,b)) / 2.0;
+}
+
+function setCardHandlers(radio, cardList, propertyName) {
+    // Update the cards when the corresponding form is changed
+    for (var i = 0; i < radio.length; i++) {
+        radio[i].addEventListener("change", function() {
+            document.documentElement.style.setProperty(propertyName, "rgb(" + this.value + ")");
+            var channels = this.value.split(",");
+            setCardBase(cardList, lightness(channels));
+        });
+    }
+}
--- app/tester.py
+++ app/tester.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 # Misc Render Function for testing templating
 @app.route("/", methods=['GET', 'POST'])
 def test():
-    return render_template("game-setup.html")
+    test_colors = ["#000000", "#f5493d", "#318fe0", "#78a6ce"]
+    test_rgb = ["238,58,58", "28,28,28", "255,255,255", "78, 78, 245"]
+    return render_template("game-setup.html", colors=test_rgb)
 
 if __name__ == "__main__":
     app.debug = True
