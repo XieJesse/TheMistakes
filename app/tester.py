df3fe5151dfff9c800bcb1d34abfe2f49417588c
sookiemonster <sookiemonster216@gmail.com>
2021-12-22 08:55:00 -0500

    Rename setup file

--- app/tester.py
+++ app/tester.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 # Misc Render Function for testing templating
 @app.route("/", methods=['GET', 'POST'])
 def test():
-    test_colors = ["#000000", "#f5493d", "#318fe0", "#78a6ce"]
-    test_rgb = ["238,58,58", "28,28,28", "255,255,255", "78, 78, 245"]
-    return render_template("game-setup.html", colors=test_rgb)
+    # test_colors = ["#000000", "#f5493d", "#318fe0", "#78a6ce"]
+    # test_rgb = ["238,58,58", "28,28,28", "255,255,255", "78, 78, 245"]
+    # return render_template("setup.html", colors=test_rgb)
+    return render_template("game.html")
 
 if __name__ == "__main__":
     app.debug = True
