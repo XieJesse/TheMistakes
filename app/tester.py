96b26464c65042fa840b4cf5a0950884516d1cc9
sookiemonster <sookiemonster216@gmail.com>
2021-12-28 20:02:07 -0500

    Add leaderboard template

--- app/tester.py
+++ app/tester.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, render_template, redirect
 import sqlite3
 import json
+import db
 
 app = Flask(__name__)
 
@@ -10,10 +11,22 @@ def test():
     # test_colors = ["#000000", "#f5493d", "#318fe0", "#78a6ce"]
     # test_rgb = ["238,58,58", "28,28,28", "255,255,255", "78, 78, 245"]
     # return render_template("setup.html", colors=test_rgb)
-    test_cards = [["A", "DIAMONDS"], ["3", "CLUBS"], ]
-    test_cpus = [[["A", "DIAMONDS"], ["3", "CLUBS"]], [["A", "DIAMONDS"], ["3", "CLUBS"], ["3", "CLUBS"]]]
+    # test_cards = [["A", "DIAMONDS"], ["3", "CLUBS"], ]
+    # test_cpus = [[["A", "DIAMONDS"], ["3", "CLUBS"]], [["A", "DIAMONDS"], ["3", "CLUBS"], ["3", "CLUBS"]]]
 
-    return render_template("game.html", cards = test_cards, cpus = test_cpus)
+    # return render_template("game.html", cards = test_cards, cpus = test_cpus)
+    
+    d = db.get_db()
+    c = d.cursor()
+
+    GET_PLAYERS = """SELECT * FROM USERS"""
+
+    c.execute(GET_PLAYERS)
+
+    test_players = c.fetchall()
+    print(test_players[0]['profile_picture'])
+
+    return render_template("leaderboard.html", players=test_players)
 
 if __name__ == "__main__":
     app.debug = True
