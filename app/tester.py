a6a9ce4c5457ed531b2287fd53bfbeceb783710c
sookiemonster <sookiemonster216@gmail.com>
2021-12-29 23:28:10 -0500

    Update shop template buy layout

--- app/tester.py
+++ app/tester.py
@@ -8,25 +8,40 @@ app = Flask(__name__)
 # Misc Render Function for testing templating
 @app.route("/", methods=['GET', 'POST'])
 def test():
+
+    # SETUP TEST
     # test_colors = ["#000000", "#f5493d", "#318fe0", "#78a6ce"]
     # test_rgb = ["238,58,58", "28,28,28", "255,255,255", "78, 78, 245"]
     # return render_template("setup.html", colors=test_rgb)
+
+    # GAME TEST
     # test_cards = [["A", "DIAMONDS"], ["3", "CLUBS"], ]
     # test_cpus = [[["A", "DIAMONDS"], ["3", "CLUBS"]], [["A", "DIAMONDS"], ["3", "CLUBS"], ["3", "CLUBS"]]]
 
     # return render_template("game.html", cards = test_cards, cpus = test_cpus)
     
+    # LEADERBOARD TEST
+    # d = db.get_db()
+    # c = d.cursor()
+
+    # GET_PLAYERS = """SELECT * FROM USERS"""
+
+    # c.execute(GET_PLAYERS)
+
+    # test_players = c.fetchall()
+    # print(test_players[0]['profile_picture'])
+
+    # return render_template("leaderboard.html", players=test_players)
+
+    # SHOP TEST
+
     d = db.get_db()
     c = d.cursor()
+    
+    c.execute("SELECT * FROM SHOP")
 
-    GET_PLAYERS = """SELECT * FROM USERS"""
-
-    c.execute(GET_PLAYERS)
-
-    test_players = c.fetchall()
-    print(test_players[0]['profile_picture'])
-
-    return render_template("leaderboard.html", players=test_players)
+    test_items = c.fetchall()
+    return render_template("shop.html", items=test_items)
 
 if __name__ == "__main__":
     app.debug = True
