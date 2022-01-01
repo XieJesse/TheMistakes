947581022330c8fe95418c4357b7b71189bd6b21
JesseStuy20 <jxie20@stuy.edu>
2022-01-01 16:44:01 -0500

    Added route to profile html

--- app/__init__.py
+++ app/__init__.py
@@ -46,6 +46,34 @@ def home():
     else:
         return auth.login()
 
+@app.route("/profile", methods=['GET', 'POST'])
+@auth.login_required
+def profile():
+    d = db.get_db()
+    c = d.cursor()
+    c.execute("SELECT * FROM USERS WHERE USERNAME = (?)", (session['username'],))
+    userData = c.fetchone()
+    balance = userData[2]
+    wins = userData[3]
+    currColor = userData[7]
+    colors = []
+    pfps = []
+    inventory_file = f"{session['username']}.txt"
+    inventory_dir = "inventories"
+
+    # Join the inventory filename to the inventories path
+    inventory_path = os.path.join(inventory_dir, inventory_file)
+
+    with open(inventory_path, "r") as inventory:
+        allItems = inventory.readlines()
+    for item in allItems:
+        itemList = item.split(",")
+        if itemList[0] == "card_color":
+            colors.append(itemList[2])
+        if itemList[0] == "pfp":
+            pfps.append(itemList[2])
+    return render_template("profile.html",username=session['username'],balance=balance,wins=wins,picture_list=pfps,color_list=colors,current_color=currColor)
+
 @app.route("/shop", methods=['GET', 'POST'])
 @auth.login_required
 def shop():
